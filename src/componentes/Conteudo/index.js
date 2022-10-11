461cf5e71e17c3821e3f0429f6b25013fde6943f
vitorfail <vitor_andrademanoel@hotmail.com>
2022-10-11 19:46:42 -0300

    adicionar paleta de corer

--- src/componentes/Conteudo/index.js
+++ src/componentes/Conteudo/index.js
@@ -7,6 +7,7 @@ import Dow from '../../dowload.png'
 import Est1 from '../../estilo1.png'
 import Est2 from '../../estilo2.png'
 import Eng from '../../engrenagem.png'
+import { SketchPicker } from 'react-color'
 
 export default function Conteudo(){
     const [ imagem, setimagem] = useState(<img  className='principal' alt="perfil2"  style={{width:'90%', height:'90%'}} src=''></img>)
@@ -16,10 +17,19 @@ export default function Conteudo(){
     const [data, setdata] = useState('Nada')
     const [conteudo, setconteudo] = useState('Nada')
     const [tipo, settipo] = useState('tipo2')
-    const [rgb, setrgb] = useState([0, 0,5])
+    const [rgb, setrgb] = useState("#37d67a");
     const [download, setdownload] = useState(null)
     useEffect(() =>{
     })
+    function hexToRgb(hex) {
+        hex = hex.replace('#', '')
+        var bigint = parseInt(hex, 16);
+        var r = (bigint >> 16) & 255;
+        var g = (bigint >> 8) & 255;
+        var b = bigint & 255;
+    
+        return [r,g , b]
+    }
     function filtroConteudo(e){
         if(e.length> 78){
             e = e.match(/.{1,78}/g)
@@ -45,7 +55,7 @@ export default function Conteudo(){
         data:data,
         conteudo:filtroConteudo(conteudo),
          tipo:tipo,
-        rgb:rgb},
+        rgb:hexToRgb(rgb)},
         {responseType: 'blob'})
         .then(res => {
             console.log("Minha experiência de trabalho fez de mim mais resolutivo e proativo. Tendo que".length)
@@ -72,7 +82,7 @@ export default function Conteudo(){
                     <img alt='style' src={Est2}></img>
                     <img alt='style' src={Est1}></img>
             </div>
-            <div style={{display:'flex', justifyContent:'center',alignItems:'center', height:'40%', width:'100%'}}>
+            <div style={{display:'flex', justifyContent:'center',alignItems:'center', height:'15%', width:'100%'}}>
                 <img className='modelo' src={Cert2} alt='Certificado2'></img>
                 <img className='modelo' src={Cert3} alt='Certificado3'></img>
             </div>
@@ -85,168 +95,34 @@ export default function Conteudo(){
                     <div className='box'>
                         <h3>Formando: </h3>
                         <div className='linha'>
-                            <h4>Nome:</h4>
                             <input onChange={(event) => setnome(event.target.value)} placeholder='José Manoel'></input>
-                        </div>
-                        <div className='linha'>
-                            <h4>Fonte:</h4>
-                            <select>
-                                <option className='BauerBodoniStd-Roman' value={"BauerBodoniStd-Roman"}>BauerBodoniStd-Roman</option>
-                                <option className='BODONI BK BT BOOK ITALIC' value={"BODONI BK BT BOOK ITALIC"}>BODONI BK BT BOOK ITALIC</option>
-                                <option className='BODONI BK BT BOOK' value={"BODONI BK BT BOOK"}>BODONI BK BT BOOK</option>
-                                <option className='cac_champagne' value={"cac_champagne"}>cac_champagne</option>
-                                <option className='Net-WT-Hilton-Script-Regular' value={"Net-WT-Hilton-Script-Regular"}>Net-WT-Hilton-Script-Regular</option>
-                                <option className='Roboto-Black' value={"Roboto-Black"}>Roboto-Black</option>
-                                <option className='Roboto-BlackItalic' value={"Roboto-BlackItalic"}>Roboto-BlackItalic</option>
-                                <option className='Roboto-Bold' value={"Roboto-Bold"}>Roboto-Bold</option>
-                                <option className='Roboto-BoldCondensed' value={"Roboto-BoldCondensed"}>Roboto-BoldCondensed</option>
-                                <option className='Roboto-BoldCondensedItalic' value={"Roboto-BoldCondensedItalic"}>Roboto-BoldCondensedItalic</option>
-                                <option className='Roboto-BoldItalic' value={"Roboto-BoldItalic"}>Roboto-BoldItalic</option>
-                                <option className='Roboto-Condensed' value={"Roboto-Condensed"}>Roboto-Condensed</option>
-                                <option className='Roboto-CondensedItalic' value={"Roboto-CondensedItalic"}>Roboto-CondensedItalic</option>
-                                <option className='Roboto-Italic' value={"Roboto-Italic"}>Roboto-Italic</option>
-                                <option className='Roboto-Light' value={"Roboto-Light"}>Roboto-Light</option>
-                                <option className='Roboto-LightItalic' value={"Roboto-LightItalic"}>Roboto-LightItalic</option>
-                                <option className='Roboto-Medium' value={"Roboto-Medium"}>Roboto-Medium</option>
-                                <option className='Roboto-MediumItalic' value={"Roboto-MediumItalic"}>Roboto-MediumItalic</option>
-                                <option className='Roboto-Regular' value={"Roboto-Regular"}>Roboto-Regular</option>
-                                <option className='Roboto-Thin' value={"Roboto-Thin"}>Roboto-Thin</option>
-                                <option className='Roboto-ThinItalic' value={"Roboto-ThinItalic"}>Roboto-ThinItalic</option>
-                            </select>
                         </div>
                     </div>
                     <div className='box'>
                         <h3>Diretor: </h3>
                         <div className='linha'>
-                            <h4>Nome:</h4>
                             <input onChange={(event) => setdiretor(event.target.value)} placeholder='José Manoel'></input>
-                        </div>
-                        <div className='linha'>
-                            <h4>Fonte:</h4>
-                            <select>
-                                <option className='BauerBodoniStd-Roman' value={"BauerBodoniStd-Roman"}>BauerBodoniStd-Roman</option>
-                                <option className='BODONI BK BT BOOK ITALIC' value={"BODONI BK BT BOOK ITALIC"}>BODONI BK BT BOOK ITALIC</option>
-                                <option className='BODONI BK BT BOOK' value={"BODONI BK BT BOOK"}>BODONI BK BT BOOK</option>
-                                <option className='cac_champagne' value={"cac_champagne"}>cac_champagne</option>
-                                <option className='Net-WT-Hilton-Script-Regular' value={"Net-WT-Hilton-Script-Regular"}>Net-WT-Hilton-Script-Regular</option>
-                                <option className='Roboto-Black' value={"Roboto-Black"}>Roboto-Black</option>
-                                <option className='Roboto-BlackItalic' value={"Roboto-BlackItalic"}>Roboto-BlackItalic</option>
-                                <option className='Roboto-Bold' value={"Roboto-Bold"}>Roboto-Bold</option>
-                                <option className='Roboto-BoldCondensed' value={"Roboto-BoldCondensed"}>Roboto-BoldCondensed</option>
-                                <option className='Roboto-BoldCondensedItalic' value={"Roboto-BoldCondensedItalic"}>Roboto-BoldCondensedItalic</option>
-                                <option className='Roboto-BoldItalic' value={"Roboto-BoldItalic"}>Roboto-BoldItalic</option>
-                                <option className='Roboto-Condensed' value={"Roboto-Condensed"}>Roboto-Condensed</option>
-                                <option className='Roboto-CondensedItalic' value={"Roboto-CondensedItalic"}>Roboto-CondensedItalic</option>
-                                <option className='Roboto-Italic' value={"Roboto-Italic"}>Roboto-Italic</option>
-                                <option className='Roboto-Light' value={"Roboto-Light"}>Roboto-Light</option>
-                                <option className='Roboto-LightItalic' value={"Roboto-LightItalic"}>Roboto-LightItalic</option>
-                                <option className='Roboto-Medium' value={"Roboto-Medium"}>Roboto-Medium</option>
-                                <option className='Roboto-MediumItalic' value={"Roboto-MediumItalic"}>Roboto-MediumItalic</option>
-                                <option className='Roboto-Regular' value={"Roboto-Regular"}>Roboto-Regular</option>
-                                <option className='Roboto-Thin' value={"Roboto-Thin"}>Roboto-Thin</option>
-                                <option className='Roboto-ThinItalic' value={"Roboto-ThinItalic"}>Roboto-ThinItalic</option>
-                            </select>
                         </div>
                     </div>
                     <div className='box'>
                         <h3>Reitor: </h3>
                         <div className='linha'>
-                            <h4>Nome:</h4>
-                            <input onChange={(event) => setreitor(event.target.value)} placeholder='José Manoel'></input>
-                        </div>
-                        <div className='linha'>
-                            <h4>Fonte:</h4>
-                            <select>
-                                <option className='BauerBodoniStd-Roman' value={"BauerBodoniStd-Roman"}>BauerBodoniStd-Roman</option>
-                                <option className='BODONI BK BT BOOK ITALIC' value={"BODONI BK BT BOOK ITALIC"}>BODONI BK BT BOOK ITALIC</option>
-                                <option className='BODONI BK BT BOOK' value={"BODONI BK BT BOOK"}>BODONI BK BT BOOK</option>
-                                <option className='cac_champagne' value={"cac_champagne"}>cac_champagne</option>
-                                <option className='Net-WT-Hilton-Script-Regular' value={"Net-WT-Hilton-Script-Regular"}>Net-WT-Hilton-Script-Regular</option>
-                                <option className='Roboto-Black' value={"Roboto-Black"}>Roboto-Black</option>
-                                <option className='Roboto-BlackItalic' value={"Roboto-BlackItalic"}>Roboto-BlackItalic</option>
-                                <option className='Roboto-Bold' value={"Roboto-Bold"}>Roboto-Bold</option>
-                                <option className='Roboto-BoldCondensed' value={"Roboto-BoldCondensed"}>Roboto-BoldCondensed</option>
-                                <option className='Roboto-BoldCondensedItalic' value={"Roboto-BoldCondensedItalic"}>Roboto-BoldCondensedItalic</option>
-                                <option className='Roboto-BoldItalic' value={"Roboto-BoldItalic"}>Roboto-BoldItalic</option>
-                                <option className='Roboto-Condensed' value={"Roboto-Condensed"}>Roboto-Condensed</option>
-                                <option className='Roboto-CondensedItalic' value={"Roboto-CondensedItalic"}>Roboto-CondensedItalic</option>
-                                <option className='Roboto-Italic' value={"Roboto-Italic"}>Roboto-Italic</option>
-                                <option className='Roboto-Light' value={"Roboto-Light"}>Roboto-Light</option>
-                                <option className='Roboto-LightItalic' value={"Roboto-LightItalic"}>Roboto-LightItalic</option>
-                                <option className='Roboto-Medium' value={"Roboto-Medium"}>Roboto-Medium</option>
-                                <option className='Roboto-MediumItalic' value={"Roboto-MediumItalic"}>Roboto-MediumItalic</option>
-                                <option className='Roboto-Regular' value={"Roboto-Regular"}>Roboto-Regular</option>
-                                <option className='Roboto-Thin' value={"Roboto-Thin"}>Roboto-Thin</option>
-                                <option className='Roboto-ThinItalic' value={"Roboto-ThinItalic"}>Roboto-ThinItalic</option>
-                            </select>
+                            <input onChange={(event) => setreitor(event.target.value)} placeholder='Alberto calisto'></input>
                         </div>
                     </div>
                     <div className='box'>
                         <h3>Conteudo: </h3>
                         <div className='linha'>
-                            <h4>Nome:</h4>
-                            <input onChange={(event) => setconteudo(event.target.value)} placeholder='José Manoel'></input>
-                        </div>
-                        <div className='linha'>
-                            <h4>Fonte:</h4>
-                            <select>
-                                <option className='BauerBodoniStd-Roman' value={"BauerBodoniStd-Roman"}>BauerBodoniStd-Roman</option>
-                                <option className='BODONI BK BT BOOK ITALIC' value={"BODONI BK BT BOOK ITALIC"}>BODONI BK BT BOOK ITALIC</option>
-                                <option className='BODONI BK BT BOOK' value={"BODONI BK BT BOOK"}>BODONI BK BT BOOK</option>
-                                <option className='cac_champagne' value={"cac_champagne"}>cac_champagne</option>
-                                <option className='Net-WT-Hilton-Script-Regular' value={"Net-WT-Hilton-Script-Regular"}>Net-WT-Hilton-Script-Regular</option>
-                                <option className='Roboto-Black' value={"Roboto-Black"}>Roboto-Black</option>
-                                <option className='Roboto-BlackItalic' value={"Roboto-BlackItalic"}>Roboto-BlackItalic</option>
-                                <option className='Roboto-Bold' value={"Roboto-Bold"}>Roboto-Bold</option>
-                                <option className='Roboto-BoldCondensed' value={"Roboto-BoldCondensed"}>Roboto-BoldCondensed</option>
-                                <option className='Roboto-BoldCondensedItalic' value={"Roboto-BoldCondensedItalic"}>Roboto-BoldCondensedItalic</option>
-                                <option className='Roboto-BoldItalic' value={"Roboto-BoldItalic"}>Roboto-BoldItalic</option>
-                                <option className='Roboto-Condensed' value={"Roboto-Condensed"}>Roboto-Condensed</option>
-                                <option className='Roboto-CondensedItalic' value={"Roboto-CondensedItalic"}>Roboto-CondensedItalic</option>
-                                <option className='Roboto-Italic' value={"Roboto-Italic"}>Roboto-Italic</option>
-                                <option className='Roboto-Light' value={"Roboto-Light"}>Roboto-Light</option>
-                                <option className='Roboto-LightItalic' value={"Roboto-LightItalic"}>Roboto-LightItalic</option>
-                                <option className='Roboto-Medium' value={"Roboto-Medium"}>Roboto-Medium</option>
-                                <option className='Roboto-MediumItalic' value={"Roboto-MediumItalic"}>Roboto-MediumItalic</option>
-                                <option className='Roboto-Regular' value={"Roboto-Regular"}>Roboto-Regular</option>
-                                <option className='Roboto-Thin' value={"Roboto-Thin"}>Roboto-Thin</option>
-                                <option className='Roboto-ThinItalic' value={"Roboto-ThinItalic"}>Roboto-ThinItalic</option>
-                            </select>
+                            <input onChange={(event) => setconteudo(event.target.value)} placeholder='Manoel andrade'></input>
                         </div>
                     </div>
                     <div className='box'>
                         <h3>Data: </h3>
                         <div className='linha'>
-                            <h4>Data:</h4>
-                            <input onChange={(event) => setdata(event.target.value)} placeholder='José Manoel'></input>
-                        </div>
-                        <div className='linha'>
-                            <h4>Fonte:</h4>
-                            <select>
-                                <option className='BauerBodoniStd-Roman' value={"BauerBodoniStd-Roman"}>BauerBodoniStd-Roman</option>
-                                <option className='BODONI BK BT BOOK ITALIC' value={"BODONI BK BT BOOK ITALIC"}>BODONI BK BT BOOK ITALIC</option>
-                                <option className='BODONI BK BT BOOK' value={"BODONI BK BT BOOK"}>BODONI BK BT BOOK</option>
-                                <option className='cac_champagne' value={"cac_champagne"}>cac_champagne</option>
-                                <option className='Net-WT-Hilton-Script-Regular' value={"Net-WT-Hilton-Script-Regular"}>Net-WT-Hilton-Script-Regular</option>
-                                <option className='Roboto-Black' value={"Roboto-Black"}>Roboto-Black</option>
-                                <option className='Roboto-BlackItalic' value={"Roboto-BlackItalic"}>Roboto-BlackItalic</option>
-                                <option className='Roboto-Bold' value={"Roboto-Bold"}>Roboto-Bold</option>
-                                <option className='Roboto-BoldCondensed' value={"Roboto-BoldCondensed"}>Roboto-BoldCondensed</option>
-                                <option className='Roboto-BoldCondensedItalic' value={"Roboto-BoldCondensedItalic"}>Roboto-BoldCondensedItalic</option>
-                                <option className='Roboto-BoldItalic' value={"Roboto-BoldItalic"}>Roboto-BoldItalic</option>
-                                <option className='Roboto-Condensed' value={"Roboto-Condensed"}>Roboto-Condensed</option>
-                                <option className='Roboto-CondensedItalic' value={"Roboto-CondensedItalic"}>Roboto-CondensedItalic</option>
-                                <option className='Roboto-Italic' value={"Roboto-Italic"}>Roboto-Italic</option>
-                                <option className='Roboto-Light' value={"Roboto-Light"}>Roboto-Light</option>
-                                <option className='Roboto-LightItalic' value={"Roboto-LightItalic"}>Roboto-LightItalic</option>
-                                <option className='Roboto-Medium' value={"Roboto-Medium"}>Roboto-Medium</option>
-                                <option className='Roboto-MediumItalic' value={"Roboto-MediumItalic"}>Roboto-MediumItalic</option>
-                                <option className='Roboto-Regular' value={"Roboto-Regular"}>Roboto-Regular</option>
-                                <option className='Roboto-Thin' value={"Roboto-Thin"}>Roboto-Thin</option>
-                                <option className='Roboto-ThinItalic' value={"Roboto-ThinItalic"}>Roboto-ThinItalic</option>
-                            </select>
+                            <input onChange={(event) => setdata(event.target.value)} placeholder='12/02/2020'></input>
                         </div>
                     </div>
+                    <SketchPicker disableAlpha={true} color={rgb} height='200px' width='91%' onChange={(color) => setrgb(color.hex)} />
                 </div>
                 <div className='container' >
                     {download}
