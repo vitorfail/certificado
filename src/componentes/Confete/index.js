222b643f8dbd90252c5542fd9fe18cb39bff19e2
vitorfail <vitor_andrademanoel@hotmail.com>
2024-02-10 11:39:42 -0300

    testadno um novo efeito

---
+++ src/componentes/Confete/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import React, { useEffect, useState } from 'react'
+import Eng from '../../engrenagem.png'
+
+export default function Confete(){
+    return(
+        <div>
+        </div>
+    )
+}
